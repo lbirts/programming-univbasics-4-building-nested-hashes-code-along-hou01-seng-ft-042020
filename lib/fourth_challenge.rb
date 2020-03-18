fedeb3ba476a71348421b145b25fa56a819704c4
Lauren Birts <birtslauren@gmail.com>
2020-03-18 04:58:49 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -8,7 +8,7 @@ def fourth_challenge
         #your hashes here!
         { :name => "Benvolio",
           :age => "17",
-          :attitude => "worried"}
+          :attitude => "worried"},
         { :name => "Mercutio",
           :age => "18",
           :attitude => "hot-headed"}
@@ -22,7 +22,7 @@ def fourth_challenge
         #your hashes here!
         { :name => "Steven",
           :age => "30",
-          :attitude => "confused"}
+          :attitude => "confused"},
         { :name => "Nurse",
           :age => "44",
           :attitude => "worried"}
